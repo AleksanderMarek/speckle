b355592e5f5753d4a1afebbb1157daa84bc02ad9
AleksanderMarek <aleksander.marek.pl@gmail.com>
2023-01-14 12:19:18 +0000

    Added a function that splits the blender image into 500 x 500 px tiles

---
+++ cropBlenderImages.py
@@ -0,0 +1,6 @@
+import speckle
+
+im_input = r"E:\GitHub\speckle\development\blender\test_14.tiff"
+output_folder = "E:\\GitHub\\speckle\\input_images"
+
+speckle.trim_render_im(im_input, output_folder)
--- generateSpecklePattern.py
+++ generateSpecklePattern.py
@@ -1,12 +1,25 @@
 import speckle
 
+""" This script generates training images for qe-net. First a synthetic
+speckle pattern is generated using a FFT-based algorithm. Then, the pattern
+is used in blender to simulate more realistic experimental conditions such as
+lighting, blur etc. Finally, the image rendered in blender is split into 
+500 x 500 px training images that will be used to calculate noise floor and 
+train the net
+"""
+
+""" To do:
+    -Develop robust loging of generation parameters so that each training image
+    can be linked directly to the optimised speckle generator input and 
+    blender input
+"""    
 
 image_size = (1000, 1000)
 speckle_size = 4;
 fft_threshold = 4
 output_DPI = 600
 target_size = (100, 100)
-target_path = 'E:\\GitHub\\speckle\\images\\im4.tiff'
+target_path = 'E:\\GitHub\\speckle\\speckle_images\\im4.tiff'
 
 imaging_dist = 1000
 focal_length = 50
