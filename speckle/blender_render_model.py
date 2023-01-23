22c9807c5dec78d7650a11c88f2b792f7a8265ae
Aleksander Marek <aleksander.marek.pl@gmail.com>
2023-01-23 20:16:47 +0000

    Changed output to 100x100 px

--- speckle/blender_render_model.py
+++ speckle/blender_render_model.py
@@ -118,9 +118,10 @@ def blender_render_model(output_path, pattern_path, normal_map_path):
     cam1.dof.use_dof = True
     cam1.dof.aperture_fstop = fstop
     cam1.lens = focal_length
-    cam1.sensor_width = 8.80
-    cam1.sensor_height = 6.60
-    cam1.dof.focus_object = bpy.data.objects["Cube"]
+    cam1.sensor_width = 8.46
+    cam1.sensor_height = 7.09
+    # cam1.dof.focus_object = bpy.data.objects["Cube"]
+    cam1.dof.focus_distance = 1.0
     
     # Add cross camera
     cross_angle = math.radians(20)
@@ -204,8 +205,8 @@ def blender_render_model(output_path, pattern_path, normal_map_path):
     scene = bpy.context.scene
     scene.camera = camera
     scene.render.filepath = output_path
-    scene.render.resolution_x = 2448
-    scene.render.resolution_y = 2048
+    scene.render.resolution_x = 2452
+    scene.render.resolution_y = 2056
     scene.render.image_settings.file_format = 'TIFF'
     scene.render.image_settings.color_mode = 'BW'
     scene.render.image_settings.color_depth = '8'
