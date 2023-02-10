ddce5df9b5670ddc339a097132291b1c98ed1867
Aleksander Marek <aleksander.marek.pl@gmail.com>
2023-02-10 15:39:37 +0000

    Fixed cross camera settings

--- speckle/blender_render_model.py
+++ speckle/blender_render_model.py
@@ -133,13 +133,13 @@ def blender_render_model(output_path, pattern_path, normal_map_path):
     camera2.location = (cross_dist*math.sin(cross_angle), 
                         -cross_dist*math.cos(cross_angle), 
                         0.0)
-    camera2.rotation_euler = (math.pi/2, 0, math.sin(cross_angle))
+    camera2.rotation_euler = (math.pi/2, 0, cross_angle)
     cam2.dof.use_dof = True
     cam2.dof.aperture_fstop = fstop
     cam2.lens = focal_length
-    cam2.sensor_width = 8.80
-    cam2.sensor_height = 6.60
-    cam2.dof.focus_object = bpy.data.objects["Cube"]
+    cam2.sensor_width = 8.46
+    cam2.sensor_height = 7.09
+    cam2.dof.focus_distance = 1.0
     
     # Make a new material
     mat = bpy.data.materials.new(name="Material")
