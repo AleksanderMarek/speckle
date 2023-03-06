f3154f41a5fe506fa7dff100d05a56b079a1698f
Aleksander Marek <aleksander.marek.pl@gmail.com>
2023-03-06 21:03:10 +0000

    Cleaned up scene setup

--- render_validation_specimen.py
+++ render_validation_specimen.py
@@ -24,7 +24,7 @@ displ_filepath = [r"D:\GitHub\speckle\test_specimen\fullSpec1.csv",
 # displ_filepath = []
 
 
-# Generate speckle pattern
+# SETTINGS
 image_size = (2000, 3500)
 target_size = (70, 160)
 pixel_size_physical = 0.00345  # mm
@@ -35,31 +35,33 @@ pixel_size = pixel_size_physical/M
 speckle_size = 4.0 * pixel_size
 output_DPI = 600
 n_samples = 50
-# Check if the size of the speckle pattern is sufficient
-if (output_DPI/25.4*target_size[0] > image_size[0]) or \
-        (output_DPI/25.4*target_size[1] > image_size[1]):
-    print("Warning: the resolution of the speckle pattern is too small!")
+
+# GENERATE SPECKLES
 pat1 = speckle.SpeckleImage(image_size, speckle_size)
 pat1.set_physical_dim(target_size, speckle_size, output_DPI)
-# im1 = pat1.gen_pattern()
-# pat1.pattern_gradient()
-# pat1.im_save(pattern_path)
+im1 = pat1.gen_pattern()
+pat1.pattern_gradient()
+pat1.im_save(pattern_path)
 # Generate normals map
-# normals_map = speckle.SpeckleImage(pat1.image_size, 60, normal_map_filter=1)
-# normals_map.gen_pattern()
-# normals_map.pattern_gradient()
-# normals_map.generate_norm_map(binary_map=pat1.gradient)
-# normals_map.grad_save(normals_path)
+normals_map = speckle.SpeckleImage(pat1.image_size, 60, normal_map_filter=1)
+normals_map.gen_pattern()
+normals_map.pattern_gradient()
+normals_map.generate_norm_map(binary_map=pat1.gradient)
+normals_map.grad_save(normals_path)
 
 # Generate blender scene
 a = VirtExp(pattern_path, normals_path, output_path, model_path,
             objects_position="fixed")
+
 # Set up the scene
 # Get default properties
 p = a.get_default_params()
-# CAMERAS
+
+# PART
 # Add the default target
 target = a.add_FEA_part(mesh_path)
+
+#LIGHT
 # Add the light panel
 # Calculate desired orientation of the light
 light_target_orient = p["light_target"] - np.array(p["light_pos"])
@@ -70,21 +72,19 @@ a.add_light(p["light_type"], pos=p["light_pos"], orient=light_angle,
             energy=p["light_energy"], spot_size=p["light_spotsize"],
             spot_blend=p["light_spot_blend"],
             shadow_spot_size=p["light_shad_spot"])
+# CAMERAS
 # Add straight camera
 # Calculate desired orientation of the cam
 cam0_pos = p["cam0_pos"]
-#cam0_pos = np.array([-0.174, 0.002, 1.001])
 cam0_target_orient = p["cam0_target"] - cam0_pos
 cam0_target_dist = np.linalg.norm(cam0_target_orient)+1e-16
 cam_angle = a.calc_rot_angle(p["cam_init_rot"], cam0_target_orient)
 cam0 = a.add_camera(pos=cam0_pos, orient=cam_angle,
                     fstop=p["cam_fstop"],
                     focal_length=p["cam_foc_length"],
-                    obj_distance=cam0_target_dist,
-                    k1=0.0, p1=0.00)
+                    obj_distance=cam0_target_dist)
 # Add cross camera
 cam1_pos = p["cam1_pos"]
-#cam1_pos = np.array([0.174, -0.002, 1.015])
 cam1_target_orient = p["cam1_target"] - cam1_pos
 cam1_target_dist = np.linalg.norm(cam1_target_orient)+1e-16
 cam_angle = a.calc_rot_angle(p["cam_init_rot"], cam1_target_orient)
@@ -92,10 +92,14 @@ cam1 = a.add_camera(pos=cam1_pos, orient=cam_angle,
                     fstop=p["cam_fstop"],
                     focal_length=p["cam_foc_length"],
                     obj_distance=cam1_target_dist)
-a.rotate_around_z(cam1, 0.5)
-#a.rotate_around_z(cam0, 0.1)
+# Rotate cam1 around the z-axis (lens axis)
+#a.rotate_around_z(cam1, 0.5)
+
+# Material
 # Define the material and assign it to the cube
 a.add_material(target)
+
+# Render images
 # Write the calibration file
 a.generate_calib_file(cam0, cam1, calib_path)
 # Set the renderer up and render image
@@ -112,21 +116,21 @@ a.set_renderer(cam1, n_samples=n_samples)
 a.add_image_distortion(cam1)
 a.render_scene(output_path2)
 
-# # Deform images
-# for i, displ_file in enumerate(displ_filepath):
-#     # Update position of FE nodes
-#     a.deform_FEA_part(target, displ_file)
-#     # Add another frame to the animation
-#     a.set_new_frame(target)
-#     # Render the scene with the perpendicular camera
-#     def_path = f"D:\\Experiment Quality\\blender_model\\render_{i+1}_0.tiff"
-#     a.set_renderer(cam0, n_samples=n_samples)
-#     a.add_image_distortion(cam0)
-#     a.render_scene(def_path)
-#     # Switch the camera to the cross one and render the scene
-#     def_path = f"D:\\Experiment Quality\\blender_model\\render_{i+1}_1.tiff"
-#     a.set_renderer(cam1, n_samples=n_samples)
-#     a.add_image_distortion(cam1)
-#     a.render_scene(def_path)
-# # Save final model
-# a.save_model()
+# Deform images
+for i, displ_file in enumerate(displ_filepath):
+    # Update position of FE nodes
+    a.deform_FEA_part(target, displ_file)
+    # Add another frame to the animation
+    a.set_new_frame(target)
+    # Render the scene with the perpendicular camera
+    def_path = f"D:\\Experiment Quality\\blender_model\\render_{i+1}_0.tiff"
+    a.set_renderer(cam0, n_samples=n_samples)
+    a.add_image_distortion(cam0)
+    a.render_scene(def_path)
+    # Switch the camera to the cross one and render the scene
+    def_path = f"D:\\Experiment Quality\\blender_model\\render_{i+1}_1.tiff"
+    a.set_renderer(cam1, n_samples=n_samples)
+    a.add_image_distortion(cam1)
+    a.render_scene(def_path)
+# Save final model
+a.save_model()
