a70ff7a729f6f2ae6e537f90021bacab6d4585f7
Aleksander Marek <aleksander.marek.pl@gmail.com>
2023-03-22 22:08:21 +0000

    Removed old files

--- speckle/__init__.py
+++ speckle/__init__.py
@@ -1,5 +1,2 @@
 from .SpeckleImage import SpeckleImage
-from .trim_render_im import *
-from .blender_render_model import blender_render_model
-from .image_deformation import image_deformation
 from .VirtExp import VirtExp
